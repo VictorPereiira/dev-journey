ed359d034e9a1b6a34c37dbcb3d2b372e9521760
victor <victor.psa02@gmail.com>
2021-09-09 00:03:48 -0300

    exc_3 complete

--- use/03_obg_mentoring/class_01/exc_3.js
+++ use/03_obg_mentoring/class_01/exc_3.js
@@ -2,5 +2,14 @@ let myArray = [
     30, 5, 5, 18, 43, 16, 21,
     16, 24, 4, 38, 3, 18, 19,
     18, 42, 6, 35, 38, 43
-]
+],
+    evenToOdd = myArray.slice(),
+    lessToMajor = myArray.slice()
+
+evenToOdd = evenToOdd.sort(x => x % 2 ? 1 : -1)
+lessToMajor = myArray.sort((x, y) => {
+    return x === y ? 0 : (x < y ? -1 : 1)
+})
+
+console.log(evenToOdd, lessToMajor)
 
---
+++ use/03_obg_mentoring/class_01/exc_4.js
@@ -0,0 +1,8 @@
+let celsiusToFahrenheit = celsius => celsius * 1.8 + 32,
+    wordLength = text => text
+
+
+
+console.log(
+    celsiusToFahrenheit(23)
+)
